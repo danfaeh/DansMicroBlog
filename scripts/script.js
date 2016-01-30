7e91aabac80c2e9b9505c9f0ccaf10ba54ac6b63
danfaeh <danfaeh@gmail.com>
2016-01-29 16:21:41 -0800

    moved to dev

---
+++ scripts/script.js
@@ -0,0 +1,83 @@
+window.onload = start;
+var counter=0;
+
+  function start(){
+    restoreLS();
+    listenerPost();
+    deletepost();
+    deleteAllPosts();
+  }
+
+  function restoreLS(){
+    if (localStorage.length > 0){
+      $('#counter').html('Currently '+localStorage.length+' comment(s)');
+      for (var key in localStorage){
+        $('#list').prepend('<li id="'+key+'">'+localStorage[key]+'</li>');
+      }
+    }
+    else{
+      $('#counter').html('0 comments... Be the first!');
+    }
+  }
+
+  function listenerPost(){
+    $('#post').click(function(e){
+      e.preventDefault();
+        if ($('#comment').val() !== '') {
+          var $node = $('#comment');
+          var $comment = $('#comment').val();
+            addNewComment($comment);
+            $node.val('');
+        }   
+    });
+  }
+
+  function addNewComment(comment){
+    addGracefully(comment);
+      counter ++;
+      $("#counter").html("Currently: "+counter+" comment(s)");
+        localStorage.setItem(counter-1,comment);
+  }
+  
+  function deletepost(){
+    counter = localStorage.length;
+    $('#list').on('click', 'li', function(e){
+      removeGracefully(this);
+
+      //removal updates counter
+      counter --;
+      $("#counter").html("Currently: "+counter+" comment(s)");
+
+      //update removal in LS
+      var y = e.target.id;
+      localStorage.removeItem(y);
+    });
+  }
+
+  function deleteAllPosts(){
+    $('#clear').click(function(e){
+      localStorage.clear();
+      $('li').remove();
+      $('#counter').html('0 comments... Be the first!');
+      counter=0;
+    });
+  }
+
+  function removeGracefully(self) {
+      $(self).fadeOut(1500, function() {
+          $(self).remove();
+      });
+  }
+
+  //fade in animations not working for some reason
+  function addGracefully(comment) {
+      $(this).fadeIn(1000, function() {
+        $('#list').prepend('<li id="'+counter+'">'+comment+'</li>');
+    });
+  }
+
+
+
+
+
+
